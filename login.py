5ff1b9c7a1724cf67b2b4e731c557c640209905a
zhuyst <13553371651@139.com>
2018-06-13 16:20:17 +0800

    使用lxml获取token

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-from html.parser import HTMLParser
+from lxml import html
 import session
 
 # TMS系统登陆URL
@@ -8,46 +8,12 @@ loginUrl = "http://tm.bnuz.edu.cn/uaa/login"
 loginFailUrl = loginUrl + "?error"
 
 
-# 解析登陆页的HTML，用于获取表单中的CSRF Token
-class MyHtmlParser(HTMLParser):
-
-    def __init__(self):
-        super().__init__()
-        self._token = None
-
-    # CSRF Token
-    @property
-    def token(self):
-        return self._token
-
-    def error(self, message):
-        pass
-
-    def handle_starttag(self, tag, attrs):
-
-        # 从标签中获取属性值
-        def _attr(attrName):
-            for attr in attrs:
-                if attr[0] == attrName:
-                    return attr[1]
-            return None
-
-        # 获取表单中的CSRF Token
-        # input -> id="csrf_token":
-        if tag == "input":
-            for name, value in attrs:
-                if name == "id" and value == "csrf_token":
-                    self._token = _attr("value")
-
-
 # 获取CSRF Token，用于进行安全验证
 def getCsrfToken():
     response = session.get(loginUrl)
-    parser = MyHtmlParser()
-    parser.feed(response.text)
-    parser.close()
-
-    return parser.token
+    tree = html.fromstring(response.text)
+    token = tree.xpath("//input[@id='csrf_token']")[0].value
+    return token
 
 
 # 登陆TMS系统
